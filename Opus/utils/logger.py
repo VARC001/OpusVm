66458c76be0e77791e9cd390df6e0d0f7c671ec5
ᴋᴜɴᴀʟ <kunaalkumar0091@gmail.com>
2025-03-06 23:57:03 +0530

    Update logger.py

--- Opus/utils/logger.py
+++ Opus/utils/logger.py
@@ -8,14 +8,12 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<b><u>ᴘʟᴀʏ ʟᴏɢ</u></b>
 
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}</blockquote>
+<blockquote><b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code></blockquote>
 
-<blockquote><b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}</blockquote>"""
+<blockquote><b><u>sᴛʀᴇᴀᴍᴛʏᴘᴇ : {streamtype}</u></b></blockquote>"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
